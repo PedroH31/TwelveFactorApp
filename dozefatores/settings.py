4dff14d908c2948813d194bf36cda00032a29980
Pedro <necromancerdoidao@outlook.com>
2023-02-16 08:07:53 -0300

    classe User customizada, customizada classe UserManager, aplicadas migrações para o banco de dados
    close #16, close #17, close #11

--- dozefatores/settings.py
+++ dozefatores/settings.py
@@ -33,7 +33,7 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 CSRF_TRUSTED_ORIGINS = ['https://appdozefatores.fly.dev/']
 
-# AUTH_USER_MODEL = 'base.User'
+AUTH_USER_MODEL = 'base.User'
 
 
 # Application definition
